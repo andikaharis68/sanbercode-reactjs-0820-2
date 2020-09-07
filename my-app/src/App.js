f2005e299aae83b9f870490a7104c4bc44c1a630
Andika Haris <andikaharis68@gmail.com>
2020-09-08 00:53:08 +0700

    tugas 11 selesai

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Tugas9 from './Tugas-9/tugas9';
 import Tugas10 from './Tugas-10/tugas10';
+import Tugas11 from './Tugas-11/tugas11';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Tugas9 />
         <Tugas10 />
+        <Tugas11 />
       </div>
     );
   }
---
+++ my-app/src/Tugas-11/tugas11.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react"
+
+class Timer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: 10,
+            date: new Date(),
+            visible: true
+        }
+    }
+    tick() {
+        this.setState({
+            time: this.state.time - 1,
+            date: new Date()
+        })
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+    componentDidUpdate() {
+        if (this.state.visible === true) {
+            if (this.state.time <= 0) {
+                this.setState({
+                    visible: false
+                })
+            }
+        }
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+    render() {
+        return (
+            <div>
+                {this.state.visible && (
+                    <div style={{ margin: "auto", width: "700px" }}>
+                        <h1 style={{ float: "left" }}>Sekarang Jam: {this.state.date.toLocaleTimeString()}</h1>
+                        <h1 style={{ float: "right" }}>Hitung Mundur: {this.state.time} </h1>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export default Timer;
